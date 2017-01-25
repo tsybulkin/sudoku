6be3693bf16f5849d859edb79caf86ca85e9657c
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2017-01-25 16:40:34 +0200

    sudoku wo diagonal constraints works

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 assignments = []
 
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -38,10 +39,15 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    rows = 'ABCDEFGHI'
-    cols = '123456789'
-    return { rows[i]+cols[j]:grid[i*9+j] 
-        for i in range(9) for j in range(9) }
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
 
 
 def display(values):
@@ -60,37 +66,94 @@ def display(values):
 
 
 def eliminate(values):
-    for box in values:
-        if len(values[box]) == 1:
-            digit = values[box]
-            for peer in peers[box]:
-                values[peer] = values[peer].replace(digit,'')
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            #values[peer] = values[peer].replace(digit,'')
+            assign_value(values,peer,values[peer].replace(digit,''))
     return values
 
 
 def only_choice(values):
     for unit in unitlist:
-        for box in unit:
-            for digit in values[box]:
-                if not any( digit in values[b] 
-                                for b in unit if b != box ):
-                    values[box] = digit
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                #values[dplaces[0]] = digit
+                assign_value(values,dplaces[0],digit)
     return values
 
 
 def reduce_puzzle(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        print("-------------------")
+        display(values)
+
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
 
 def solve(grid):
     pass
 
-def search(values):
-    pass
 
+def search(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    display(values)
+    values = reduce_puzzle(values)
+    print(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Chose one of the unfilled square s with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        #new_sudoku[s] = value
+        assign_value(new_sudoku,s,value)
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+    return False
+
+
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 if __name__ == '__main__':
-    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    display(solve(grid_values(diag_sudoku_grid)))
+    #diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    diag_sudoku_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    #display(solve(grid_values(diag_sudoku_grid)))
+    res = search(grid_values(diag_sudoku_grid))
+    print(res)
+    display(res)
 
     try:
         from visualize import visualize_assignments
