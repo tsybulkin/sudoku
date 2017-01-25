fd6e1dbe963e3dbbf691a7fa2075326cbf12d8ca
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2017-01-25 19:15:05 +0200

    solve() implemented, which returns the right result for diag problem. Still rejected by grader

--- solution.py
+++ solution.py
@@ -106,7 +106,20 @@ def reduce_puzzle(values):
 
 
 def solve(grid):
-    pass
+    rows = 'ABCDEFGHI'
+    cols = '123456789'
+    boxes = cross(rows, cols)
+    row_units = [cross(r, cols) for r in rows]
+    column_units = [cross(rows, c) for c in cols]
+    diag_units = [ [ rows[i]+cols[i] for i in range(9) ],
+                    [rows[i]+cols[-i-1] for i in range(9) ] ]
+    square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+    unitlist = row_units + column_units + square_units + diag_units
+    units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+    peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+    return search(grid_values(grid))
 
 
 def search(values):
@@ -140,7 +153,6 @@ column_units = [cross(rows, c) for c in cols]
 diag_units = [ [ rows[i]+cols[i] for i in range(9) ],
                 [rows[i]+cols[-i-1] for i in range(9) ] ]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-print(diag_units)
 
 unitlist = row_units + column_units + square_units + diag_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
